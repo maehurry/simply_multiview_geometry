1e1e86942f559bbec4d0cff0fbaf345c47f0d017
maehurry <liuxin1699@gmail.com>
2024-07-22 23:10:55 +0800

    Init repository

---
+++ image/image_basic.hpp
@@ -0,0 +1,8 @@
+#ifndef _SIMPLYMVG_IMAGE_IMAGE_BASIC_HPP_
+#define _SIMPLYMVG_IMAGE_IMAGE_BASIC_HPP_
+
+namespace simplyMVG {
+    
+}  // namespace simplyMVG
+
+#endif  // _SIMPLYMVG_IMAGE_IMAGE_BASIC_HPP_
